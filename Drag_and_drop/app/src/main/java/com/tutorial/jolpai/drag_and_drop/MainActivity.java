e94fcc56f2d6cf1584f359efda197e3841ac326e
Tanim reja <tanim2reja@gmail.com>
2015-05-12 17:47:20 +0600

    second commit

--- Drag_and_drop/app/src/main/java/com/tutorial/jolpai/drag_and_drop/MainActivity.java
+++ Drag_and_drop/app/src/main/java/com/tutorial/jolpai/drag_and_drop/MainActivity.java
@@ -1,17 +1,124 @@
 package com.tutorial.jolpai.drag_and_drop;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private ImageView imageView;
+    private static final String IMAGEVIEW_TAG = "Android Logo";
+    private android.widget.RelativeLayout.LayoutParams layoutParams;
+    String msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setTag(IMAGEVIEW_TAG);
+
+    }
+
+
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            ClipData clipData = ClipData.newPlainText("", "");
+            View.DragShadowBuilder dsb = new View.DragShadowBuilder(view);
+            view.startDrag(clipData, dsb, view, 0);
+            view.setVisibility(View.INVISIBLE);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    @Override
+    protected  void onResume(){
+        super.onResume();
+
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+
+                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+                ClipData dragData = new ClipData(v.getTag().toString(),
+                        mimeTypes, item);
+
+                // Instantiates the drag shadow builder.
+                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(imageView);
+
+                // Starts the drag
+                v.startDrag(dragData,  // the data to be dragged
+                        myShadow,  // the drag shadow builder
+                        null,      // no need to use local data
+                        0          // flags (not currently used, set to 0)
+                );
+                return true;
+            }
+        });
+
+
+        imageView.setOnDragListener( new View.OnDragListener(){
+            @Override
+            public boolean onDrag(View v,  DragEvent event){
+                switch(event.getAction())
+                {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        layoutParams = (RelativeLayout.LayoutParams)
+                                v.getLayoutParams();
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
+                        // Do nothing
+                        break;
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
+                        int x_cord = (int) event.getX();
+                        int y_cord = (int) event.getY();
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED :
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
+                        x_cord = (int) event.getX();
+                        y_cord = (int) event.getY();
+                        layoutParams.leftMargin = x_cord;
+                        layoutParams.topMargin = y_cord;
+                        v.setLayoutParams(layoutParams);
+                        break;
+                    case DragEvent.ACTION_DRAG_LOCATION  :
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
+                        x_cord = (int) event.getX();
+                        y_cord = (int) event.getY();
+                        break;
+                    case DragEvent.ACTION_DRAG_ENDED   :
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
+                        // Do nothing
+                        break;
+                    case DragEvent.ACTION_DROP:
+                        Log.d(msg, "ACTION_DROP event");
+                        // Do nothing
+                        break;
+                    default: break;
+                }
+                return true;
+            }
+        });
+
+
+
     }
 
     @Override
